22cbd686d3c44dd4c15359ecec6ea2d360d52f3c
kex5n <kentaro.ishii0407@gmail.com>
2022-08-26 23:34:35 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ rm -r dotfiles
 
 # install pyenv
 git clone git://github.com/yyuu/pyenv.git ~/.pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zprofile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zprofile
+echo 'eval "$(pyenv init --path)"' >> ~/.zprofile
+
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
